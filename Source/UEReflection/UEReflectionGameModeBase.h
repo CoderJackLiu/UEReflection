b7e5c0140d1ad3985ec572baeb22597e06239663
CoderJackLiu <596770421@qq.com>
2022-10-08 16:13:25 +0800

    Init UE project files

---
+++ Source/UEReflection/UEReflectionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEReflectionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEREFLECTION_API AUEReflectionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
